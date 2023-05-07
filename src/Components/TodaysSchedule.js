8cbbb84d136cdec349edd0609fcf6a8e59b19cec
movarnell <michaelvarnell@icloud.com>
2023-05-07 15:19:52 -0500

    added message for dogs there now

--- src/Components/TodaysSchedule.js
+++ src/Components/TodaysSchedule.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { format, addHours } from "date-fns";
+import { format, addHours, subHours } from "date-fns";
 
 export default function TodaysSchedule({ users, deleteUser }) {
 	console.log(users);
@@ -59,6 +59,16 @@ function sortUsers(users) {
 		}
 	}	
 
+const now = new Date();
+
+// This function is to make sure the time is within the hour the user scheduled to be there. If it is not, it will not display the alert
+function userNxtHrTime(user) {
+	const userTime = new Date(user.date);
+	const userNxtHr = addHours(userTime, 1);
+	if (now > userTime && now < userNxtHr) {
+		return true;
+	}
+}
 
 // return element for the today schedule with alerts where appropriate and sorted with next up at the top, with alerts specific to dogs coming in the next hour
 	return (
@@ -83,9 +93,16 @@ function sortUsers(users) {
 								onClick={() => deleteUser(user.id)}
 							></button>
 							<h5 className="card-title fw-bold">{user.name} is bringing</h5>
-							<h5 className="mb-2fw-bold">{user.dogName}</h5>
+							<h5 className="mb-2 fw-bold">{user.dogName}</h5>
 							<h6 className="card-text">Today at {formatTime(user.date)}</h6>
 							
+							{ userNxtHrTime(user) ? (
+								<h6 className="alert alert-info title2 border border-2 border-info fw-bold m-1 p-1 text-center">
+								<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M115.063 21.97v9.343c0 101.953 38.158 189.648 96.343 222.093v6.094c-58.186 32.445-96.344 120.14-96.344 222.094v9.344H401.81v-9.344c0-102.552-38.804-190.274-97.53-222.188V253.5c58.722-31.917 97.53-119.64 97.53-222.188V21.97H115.06zM134 40.655h248.875c-2.477 96.445-42.742 175.523-91.938 198.906l-5.343 2.532V270.844l5.344 2.53c49.193 23.383 89.456 102.438 91.937 198.876H134c2.456-95.898 42.125-175.078 90.875-198.938l5.25-2.562v-28.594l-5.25-2.562c-48.748-23.86-88.42-103.04-90.875-198.938zm213.656 86.125c-57.607 27.81-124.526 27.84-177.562 4.095C184.748 181.78 213.91 218.012 248.22 224c-1.54 2.047-2.47 4.585-2.47 7.344 0 6.76 5.488 12.25 12.25 12.25s12.25-5.49 12.25-12.25c0-2.72-.907-5.218-2.406-7.25 35.426-5.88 65.488-44.07 79.812-97.313zM258 258.626c-6.762 0-12.25 5.488-12.25 12.25s5.488 12.25 12.25 12.25 12.25-5.488 12.25-12.25-5.488-12.25-12.25-12.25zm0 39.28c-6.762 0-12.25 5.49-12.25 12.25 0 6.763 5.488 12.25 12.25 12.25s12.25-5.487 12.25-12.25c0-6.76-5.488-12.25-12.25-12.25zm0 39.533c-6.762 0-12.25 5.488-12.25 12.25 0 6.76 5.488 12.25 12.25 12.25s12.25-5.49 12.25-12.25c0-6.762-5.488-12.25-12.25-12.25zm.125 39.906c-23.21.28-46.19 25.77-75.813 75.656h153c-30.523-51.003-53.977-75.936-77.187-75.656z"></path></svg>	Scheduled for now. If you hurry you can still make it!
+								</h6>
+							) : (
+								""
+							)}
 	{parseBoolean(user.friendly) ? <h6 className="alert alert-danger fw-bolder border border-2 border-secondary m-1 p-1"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" className="mb-2 me-1" height="1.75em" width="1.75em" xmlns="http://www.w3.org/2000/svg"><path d="M193 796c0 17.7 14.3 32 32 32h574c17.7 0 32-14.3 32-32V563c0-176.2-142.8-319-319-319S193 386.8 193 563v233zm72-233c0-136.4 110.6-247 247-247s247 110.6 247 247v193H404V585c0-5.5-4.5-10-10-10h-44c-5.5 0-10 4.5-10 10v171h-75V563zm-48.1-252.5l39.6-39.6c3.1-3.1 3.1-8.2 0-11.3l-67.9-67.9a8.03 8.03 0 0 0-11.3 0l-39.6 39.6a8.03 8.03 0 0 0 0 11.3l67.9 67.9c3.1 3.1 8.1 3.1 11.3 0zm669.6-79.2l-39.6-39.6a8.03 8.03 0 0 0-11.3 0l-67.9 67.9a8.03 8.03 0 0 0 0 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l67.9-67.9c3.1-3.2 3.1-8.2 0-11.3zM832 892H192c-17.7 0-32 14.3-32 32v24c0 4.4 3.6 8 8 8h688c4.4 0 8-3.6 8-8v-24c0-17.7-14.3-32-32-32zM484 180h56c4.4 0 8-3.6 8-8V76c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v96c0 4.4 3.6 8 8 8z"></path></svg>
      This dog is not friendly with either other dogs or people. 
 								</h6>:('')}
