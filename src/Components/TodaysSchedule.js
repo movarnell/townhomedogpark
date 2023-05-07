19f9d95c9198998e29b8d24a1d3bf2804413edf3
movarnell <michaelvarnell@icloud.com>
2023-05-07 13:48:16 -0500

    working on some bugs

--- src/Components/TodaysSchedule.js
+++ src/Components/TodaysSchedule.js
@@ -32,17 +32,28 @@ function sortUsers(users) {
 	const todaySchedule = todaysUsers(users);
 	const nextHrUsers = [];
 	const nxtHr = addHours(new Date(), 1);
-	
+	const now = new Date();
 	todaySchedule.forEach((user) => {
 		const userTime = new Date(user.date);
-		if (userTime < nxtHr) {
+		if ( (now < userTime) && (userTime < nxtHr)) {
 			nextHrUsers.push(user);
 		}
 	});
 
 	const usersNxtHrCt = nextHrUsers.length;
+	console.log("file: TodaysSchedule.js:44   TodaysSchedule   usersNxtHrCt:", usersNxtHrCt)
+	console.log(nextHrUsers);
 	const filteredUsers = sortUsers(todaySchedule);
+
 	const usersNxtHr = sortUsers(nextHrUsers);
+
+	const RemTdyUsers = filteredUsers.filter((user) => {
+		const userTime = new Date(user.date);
+		if (userTime > new Date()) {
+			return user;
+		}
+	});
+
 
 	// Function that formats the time the way we want it to display
 	function formatTime(time) {
@@ -72,7 +83,7 @@ function sortUsers(users) {
 						No dogs scheduled for today yet
 						</div>) : ("")}
 			</div>
-			{filteredUsers.map((user) => (
+			{RemTdyUsers.map((user) => (
 				<div className="col-sm-12 title2">
 					<div className="card shadow mt-2 rounded-3" key={user.id}>
 						<div className="card-body">
